41a8c47a187c39d2909a21f3d60c1c6898bdb6a5
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-02-11 14:15:54 +0530

    adding middlewares to protect user

--- client/middleware.ts
+++ client/middleware.ts
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
-    // const token = request.cookies.get('uid')?.value || '';
+    const token = request.cookies.get('uid')?.value || '';
+    console.log(token)
     // const path = request.nextUrl.pathname;
     // const regexPattern = /^\/id\/.*$/;
     // if(regexPattern.test(path) && token===''){
