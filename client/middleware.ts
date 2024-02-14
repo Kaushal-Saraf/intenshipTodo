bf52d85e503b838e6ea55dd00b3ed0ab6786875d
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-02-14 10:22:10 +0530

    hello

--- client/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-export function middleware(request: NextRequest) {
-    const token = request.cookies.get('uid')?.value || '';
-}
-export const config = {
-  matcher: [`/:path*`],
-}
